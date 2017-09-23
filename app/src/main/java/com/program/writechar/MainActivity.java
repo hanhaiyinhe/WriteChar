697a7271a4f13496e94a45771f2b07d5a604240e
hanhaiyinhe <289952030@qq.com>
2017-09-23 19:43:59 +0800

    主界面背景音乐

--- app/src/main/java/com/program/writechar/MainActivity.java
+++ app/src/main/java/com/program/writechar/MainActivity.java
@@ -1,13 +1,76 @@
 package com.program.writechar;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+    static boolean isPlay = true;
+    MediaPlayer mediaPlayer;
+    Button music_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        music_btn = (Button) findViewById(R.id.btn_music);
+        PlayMusic();
+    }
+
+    private void PlayMusic() {
+        mediaPlayer = MediaPlayer.create(this, R.raw.main_music);
+        mediaPlayer.setLooping(true);
+        mediaPlayer.start();
+    }
+
+    public void OnPlay(View v) {
+        startActivity(new Intent(MainActivity.this, SelectActivity.class));
+    }
+
+    public void OnMusic(View v) {
+        if (isPlay) {
+            if(mediaPlayer != null) {
+                mediaPlayer.stop();
+                music_btn.setBackgroundResource(R.drawable.btn_music2);
+                isPlay = false;
+            }
+        } else {
+            PlayMusic();
+            music_btn.setBackgroundResource(R.drawable.btn_music1);
+            isPlay = true;
+        }
+    }
+
+    public void OnAbout(View v) {
+        startActivity(new Intent(MainActivity.this, AboutActivity.class));
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        if(isPlay) {
+            PlayMusic();
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if(mediaPlayer != null) {
+            mediaPlayer.stop();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if(mediaPlayer != null) {
+            mediaPlayer.stop();
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
     }
 }
---
+++ app/src/main/java/com/program/writechar/SelectActivity.java
@@ -0,0 +1,13 @@
+package com.program.writechar;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SelectActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select);
+    }
+}
